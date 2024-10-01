4ecec00b55c68624c19dfd5b03facce75a3612d1
Victor Nguyen <victorpublic0000@gmail.com>
2024-10-01 16:35:32 +0000

    feat: set test_run script country to vn

--- test_run/test.py
+++ test_run/test.py
@@ -1,7 +1,7 @@
 from openalpr import Alpr
 from os import getenv
 
-alpr = Alpr("fr", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
+alpr = Alpr("vn", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
 
 test = alpr.recognize_file(f"{getenv('WORKSPACE_PATH')}/test_run/1.jpg")
 
