470ee26fb4dc8ce1d73fd523e6fa89c1485682cb
Victor Nguyen <victorpublic0000@gmail.com>
2024-10-01 10:53:35 +0000

    chore: update vscode settings.json

--- test_run/test.py
+++ test_run/test.py
@@ -1,7 +1,7 @@
 from openalpr import Alpr
 from os import getenv
 
-alpr = Alpr("eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
+alpr = Alpr("fr", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
 
 test = alpr.recognize_file(f"{getenv('WORKSPACE_PATH')}/test_run/1.jpg")
 
